33b4c67abf21386013439d1dd385b422637a2c1f
Rodrigo Acosta <rodrigo.acosta@smartvalor.com>
2018-03-11 15:48:57 -0300

    more structure changes

--- ethListener/src/main/java/network/RinkebyNetwork.java
+++ ethListener/src/main/java/network/RinkebyNetwork.java
@@ -1,20 +1,18 @@
 package network;
 
 
-import contracts.ETHBridge;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.web3j.crypto.Credentials;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.DefaultBlockParameterName;
 import org.web3j.protocol.core.methods.response.EthGetBalance;
 import org.web3j.protocol.core.methods.response.Web3ClientVersion;
 import org.web3j.protocol.http.HttpService;
+import org.web3j.tx.ReadonlyTransactionManager;
+import org.web3j.tx.TransactionManager;
 
 import java.io.IOException;
 import java.math.BigInteger;
-
-
 
 
 /**
@@ -22,27 +20,30 @@ import java.math.BigInteger;
  * Represents the Ethereum test network that will be used to connect and send transactions.
  */
 
-public class Rinkeby {
-    private static final Logger log = LoggerFactory.getLogger(Rinkeby.class);
+public class RinkebyNetwork {
+    private static final Logger log = LoggerFactory.getLogger(RinkebyNetwork.class);
 
     // the Bridge contract
     private ETHBridge contract;
 
 
-    private Credentials ethCredentials;
+    private TransactionManager transactionManager;
+    // the ETH address we are using to perform calls.
+    private final String PROGRAM_ADDESS = "0x00";
 
     // my own infura network url !! Test Network !!
     private final String INFURA_NETWORK = "https://rinkeby.infura.io/iDxnNnHTWuNN9INbkvXv";
+
 
     // web3j client that will be used to connect and send transactions
     private Web3j web3j;
     private String clientVersion;
 
     /**
-     * Connects to the Rinkeby test network.
+     * Connects to the RinkebyNetwork test network.
      * @throws NetworkException
      */
-    public void connect() throws NetworkException {
+    public Web3j connect() throws NetworkException {
         web3j = Web3j.build(new HttpService(INFURA_NETWORK));
 
         // let's request the client version to make sure we are connected.
@@ -50,12 +51,14 @@ public class Rinkeby {
         try {
             web3ClientVersion = web3j.web3ClientVersion().send();
             this.clientVersion = web3ClientVersion.getWeb3ClientVersion();
-            log.info("Connected to Rinkeby at " + this.clientVersion);
+            log.info("Connected to RinkebyNetwork at " + this.clientVersion);
         } catch (IOException e) {
             this.clientVersion = null;
-            log.error("Couldn't connect to Rinkeby!");
+            log.error("Couldn't connect to RinkebyNetwork!");
             throw new NetworkException("There was an error connecting to the ethereum blockchain");
         }
+
+        return web3j;
     }
 
     /**
@@ -80,18 +83,52 @@ public class Rinkeby {
     }
 
     /**
-     * Loads from Eth blockchain our super contract
-     * @param credentials the credentials that have eth balance to pay the fee
+     * Loads the contract from the blockchain
+     * @return
      */
-    private void loadContract(Credentials credentials) throws NetworkException {
-        // let's connect if we are not.
-        if (this.clientVersion == null){
-            this.connect();
-        }
+    public ETHBridge loadContract(){
+        if (contract != null)
+            return contract;
 
-        // this token was published by me on tx 0x256775feb4e672012a3cf7b2fe502ca2ffad0a2f1fca921f7224ee33981ef2f4
-        // https://rinkeby.etherscan.io/tx/0x256775feb4e672012a3cf7b2fe502ca2ffad0a2f1fca921f7224ee33981ef2f4
-        contract = ETHBridge.load(ETHBridge.ADDRESS,this.web3j,credentials,ETHBridge.GAS_PRICE, ETHBridge.GAS_LIMIT);
-        this.ethCredentials = credentials;
+        transactionManager = new ReadonlyTransactionManager(this.web3j, this.PROGRAM_ADDESS);
+        contract = ETHBridge.load(ETHBridge.ADDRESS, this.web3j, this.transactionManager,ETHBridge.GAS_PRICE, ETHBridge.GAS_LIMIT);
+
+        return contract;
+    }
+
+
+    public void getContractEvents() throws InterruptedException, NetworkException {
+
+
+    }
+
+    /**
+     * gets the amount of required signatures specified for the contract at creation time
+     * We know this value, but we still will get it from blockchain.
+     * @return the amount of required signatures to be able to withdrawn.
+     */
+    public int getRequiredSignatures() throws NetworkException {
+        // let's make sure the contract is loaded.
+        this.getReady();
+
+        BigInteger requiredSignatures = BigInteger.ZERO;
+        try {
+            requiredSignatures = contract.requiredSignatures().send();
+        } catch (Exception e) {
+            return 0;
+        }
+        return requiredSignatures.intValue();
+    }
+
+    /**
+     * connects and loads the contract, if necesary.
+     * @throws NetworkException
+     */
+    private void getReady() throws NetworkException {
+        if (web3j == null)
+            this.connect();
+
+        if (contract == null)
+            this.loadContract();
     }
 }
--- ethListener/src/test/java/EthContractTests.java
+++ ethListener/src/test/java/EthContractTests.java
@@ -1,4 +1,79 @@
-package PACKAGE_NAME;
+import network.NetworkException;
+import network.RinkebyNetwork;
+import org.junit.Test;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameterName;
+import org.web3j.protocol.core.methods.response.EthGetBalance;
+import org.web3j.tx.ClientTransactionManager;
+import org.web3j.tx.TransactionManager;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
 
 public class EthContractTests {
+    private RinkebyNetwork network;
+    private Web3j web3j;
+    private ETHBridge contract;
+
+    /**
+     * we need to connect to rinkeby blockchain to start tests
+     * @throws NetworkException
+     */
+    @Test
+    public void connectNetworkTest() throws NetworkException {
+        network = new RinkebyNetwork();
+        this.web3j = network.connect();
+
+        assertNotNull(this.web3j);
+        assertNotNull(network.getClientVersion());
+    }
+
+    /**
+     * Let's make sure we can load the contract from the blockchain and that is valid
+     * @throws NetworkException
+     * @throws IOException
+     */
+    @Test
+    public void loadContractTest() throws NetworkException, IOException {
+        if (network == null)
+            connectNetworkTest();
+
+
+        TransactionManager transactionManager = new ClientTransactionManager(this.web3j, "");
+        contract = ETHBridge.load(ETHBridge.ADDRESS,this.web3j,transactionManager,ETHBridge.GAS_PRICE, ETHBridge.GAS_LIMIT);
+        assertNotNull(contract);
+
+        contract.setContractAddress(ETHBridge.ADDRESS);
+        assertFalse(contract.isValid());
+    }
+
+    @Test
+    public void getContractBalance() throws IOException, NetworkException {
+        if (contract == null)
+            loadContractTest();
+
+        EthGetBalance balance = web3j.ethGetBalance(contract.getContractAddress(), DefaultBlockParameterName.LATEST).send();
+        assertNotNull(balance.getBalance());
+    }
+
+    @Test
+    public void listenEvents() throws NetworkException, InterruptedException {
+        if (network == null)
+            connectNetworkTest();
+
+        network.getContractEvents();
+    }
+
+    @Test
+    public void getRequiredSignaturesTest() throws NetworkException {
+        if (network == null)
+            connectNetworkTest();
+        int i = network.getRequiredSignatures();
+        assertNotNull(i);
+        assertEquals(2,i);
+    }
+
+
 }
